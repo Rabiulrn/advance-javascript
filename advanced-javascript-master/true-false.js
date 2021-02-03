bd68cc6bd4c70a5320709dd4740407b7f0f3f8f8
Rabiul <rnrabiul@gmail.com>
2021-02-03 07:03:11 +0600

    Truthy vs falsey variable

--- advanced-javascript-master/true-false.js
+++ advanced-javascript-master/true-false.js
@@ -1,12 +1,12 @@
-//Falsy:
+//Falsy:::::::::::::::::
 // false
 // 0
 // ""
 // udenfined
 // null
 // NaN
-//Truthy
-//'0', ' ' []
+//Truthy::::::::::::::
+//'0', ' ' [] , {} (empty array or object is true)
 let name = 12;
 if(name  || name == 0){
     console.log("condition is true");
@@ -14,3 +14,12 @@ if(name  || name == 0){
 else{
     console.log("condition is false");
 }
+
+
+// const age =4;
+// if(age){ //0 asle dhukbe na.//empty string thakle o false a jabe
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
