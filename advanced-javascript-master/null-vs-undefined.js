a0ae9968ec0a3db04cdb1ab8855661b905b3ea69
Rabiul <rnrabiul@gmail.com>
2021-02-03 07:30:03 +0600

    Null vs undefined cleared for interview

--- advanced-javascript-master/null-vs-undefined.js
+++ advanced-javascript-master/null-vs-undefined.js
@@ -1,18 +1,26 @@
-let pakhi;
+let pakhi;              //avabe undefine dibe
 function add(num1, num2){
-    console.log(num1 + num2);
-    return 
+    console.log(num1 + num2);//return na dile undefine dibe
+    return             //return lekhsi but ki return dibe likhi ni taholeo undefine dibe
 }
 
 function add(num1, num2){
-    console.log(num1, num2);
+    console.log(num1, num2);//return na dile undefine dibe
 }
 
 const premik = {name:"smart dude", phone:458421};
-console.log(premik.gf);
+console.log(premik.gf); //undefine .gf nai
 
 let fun = undefined;
 console.log(fun);
 
 let ages = [2, 5, 9]
-console.log(ages[11]);
+console.log(ages[11]); //undefine
+
+// const result = add(13,82);//add(13) //parameter missing thakle oita undefine dekhabe
+// console.log(pakhi);
+// console.log(result);
+
+
+
+///null jinista explicit vabe set kore dibo...cheching korar jonno
