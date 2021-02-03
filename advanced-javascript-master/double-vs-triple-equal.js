6c480b18d7befbc1eb74c304f65d8cdf510e6290
Rabiul <rnrabiul@gmail.com>
2021-02-03 07:53:16 +0600

    double vs triple equal done

--- advanced-javascript-master/double-vs-triple-equal.js
+++ advanced-javascript-master/double-vs-triple-equal.js
@@ -1,6 +1,20 @@
-const first = 0;
-const second = false;
+const first = 0;     //1     //2
+const second = false; //true //"2"
+//0,false 1,true 2,"2" condition is true.type doesnt matter for ==.
 if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+
+
+const first = 2;
+const second = "2";
+//== sudhu value check korbe...jekono akta type ke convert kore onootar sthe milanor chesta korbe. === value and type duitai check korbe.
+if(first === second){  /// == dile condition true hoito.
     console.log("condition is true");
 }
 else{
